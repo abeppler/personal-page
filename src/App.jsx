ae554586c77a64c049749cdbc405b570c57bc455
abeppler <techstacktecnologia@gmail.com>
2022-08-08 13:01:37 -0300

    criação componentes e variables css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import MainContent from './components/MainContent'
+import Sidebar from './components/Sidebar'
+
+import './styles/components/app.sass'
+
 function App() {
   return (
-    <div className="App">
-      <h1>React + SASS</h1>
+    <div id="portfolio">
+      <h1>Anderson Beppler</h1>
+      <Sidebar />
+      <MainContent />
     </div>
   )
 }
