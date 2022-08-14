d7d9522b5d48970a3142543f1dbc345dfd0c8579
abeppler <techstacktecnologia@gmail.com>
2022-08-14 00:00:10 -0300

    inclusão das tecnologias

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -5,17 +5,19 @@ import {
   DiNodejsSmall,
   DiMsqlServer,
   DiReact,
+  DiDotnet,
 } from "react-icons/di";
 
 import "../styles/components/technologiescontainer.sass";
 
 const technologies = [
+  { id: "dotnet", name: "Dotnet", icon: <DiDotnet /> },
+  { id: "sqlserver", name: "SQL Server", icon: <DiMsqlServer /> },
+  { id: "react", name: "React", icon: <DiReact /> },
   { id: "html", name: "HTML5", icon: <DiHtml5 /> },
   { id: "css", name: "CSS3", icon: <DiCss3 /> },
   { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
   { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
-  { id: "sqlserver", name: "SQL Server", icon: <DiMsqlServer /> },
-  { id: "react", name: "React", icon: <DiReact /> },
 ];
 
 export const TechnologiesContainer = () => {
