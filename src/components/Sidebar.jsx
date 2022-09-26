b0444a02f37674198b5fe91921f9773185b2b439
abeppler <techstacktecnologia@gmail.com>
2022-09-26 12:59:27 -0300

    funcionando download pdf dinâmico

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,6 @@
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import MyResume from "./MyResume";
+
 import Avatar from "../img/me.jpg";
 import InformationContainer from "./InformationContainer";
 import SocialMedia from "./SocialMedia";
@@ -11,9 +14,14 @@ const Sidebar = () => {
       <p className="title">Fullstack Developer</p>
       <SocialMedia />
       <InformationContainer />
-      <a href="/src/assets/anderson-beppler-cv-pt.pdf" className="btn">
+      {/* <a href="/src/assets/anderson-beppler-cv-pt.pdf" className="btn">
         Download Currículo
-      </a>
+      </a> */}
+      <PDFDownloadLink document={<MyResume />} fileName="somename.pdf">
+        {({ blob, url, loading, error }) =>
+          loading ? "Loading document..." : "Download now!"
+        }
+      </PDFDownloadLink>
     </aside>
   );
 };
